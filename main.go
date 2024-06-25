e1232502f515872ef3d9aa4e70491d990867e8d1
Marcus Crestani <marcus.crestani@active-group.de>
2024-06-25 10:35:09 +0200

    Remove govvv in favour of built-in build info.
    govvv is no longer maintained.

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/magnusbaeck/logstash-filter-verifier/v2/internal/app"
 )
 
-// GitSummary contains "git describe" output and is automatically
-// populated via linker options when building with govvv.
 var GitSummary = "(unknown)"
 
 func main() {
+	info, _ := debug.ReadBuildInfo()
+	for _, kv := range info.Settings {
+		switch kv.Key {
+		case "vcs":
+			GitSummary = kv.Value + " "
+		case "vcs.revision":
+			GitSummary += kv.Value + " "
+		case "vcs.time":
+			GitSummary += kv.Value + " "
+		case "vcs.modified":
+			if kv.Value == "true" {
+				GitSummary += " dirty"
+			}
+		}
+	}
 	exitCode := app.Execute(GitSummary, os.Stdout, os.Stderr)
 	os.Exit(exitCode)
 }
